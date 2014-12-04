4e870943749e63856b58324bba14df5184ab70ab
FabienFrancois <fabien.francois@eleves.ec-nantes.Fr>
2014-12-04 10:52:05 +0100

    Complétion de la classe Case : attributs, constructeurs, getters et setters (uniquement sur le Joueur dans le cas des setters)

--- src/Plateau/Case.java
+++ src/Plateau/Case.java
@@ -5,10 +5,55 @@
  */
 package Plateau;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Fabien
  */
 public class Case {
-    
+    private int x;
+    private int y;
+    private Joueur p;
+    /**
+     * Constructeur de base de la classe Case, prenant deux entier en paramètres
+     * @param x : abscisse
+     * @param y : ordonnée
+     */
+    public Case(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.p = null;
+    }
+    /**
+     * Constructeur de recopie de la classe Case
+     * @param a : la Case à recopier
+     */
+    public Case(Case a){
+        this.x=a.x;
+        this.y=a.y;
+    }
+    /**
+     * Méthode retournant l'abscisse de la case
+     * @return l'abscisse sous forme d'un entier
+     */
+    public int getX() {
+        return x;
+    }
+    /**
+     * Méthode retournant le joueur auquel appartient la pierre posée sur la case.
+     * Si aucune pierre n'est posée, le joueur est null.
+     * @return le joueur en question.
+     */
+    public Joueur getP() {
+        return p;
+    }
+    /**
+     * Méthode retournant l'ordonnée de la case
+     * @return l'ordonnée sous forme d'un entier
+     */
+    public int getY() {
+        return y;
+    }
 }
