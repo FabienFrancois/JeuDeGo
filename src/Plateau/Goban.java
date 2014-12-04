eee2d7a8e68ab81d65707338e2bb4277f6332cdd
arigoure <arigoure@pc-b12-08.eleves.ecn>
2014-12-04 12:02:50 +0100

    Création des méthodes caseLibre, caseJouable (non terminée, le cas d'un oeil simple doit être ajouté à la méthode) et nombreDeLibertés

--- src/Plateau/Goban.java
+++ src/Plateau/Goban.java
@@ -5,6 +5,8 @@
  */
 package Plateau;
 
+import java.util.ArrayList;
+
 /**
  * Classe décrivant le goban.
  * @author Fabien
@@ -28,5 +30,45 @@ public class Goban {
     public int getTaille() {
         return taille;
     }
+    
+    /**
+     * Permet de savoir si une case est libre ou pas (si il y a déjà une pierre dessus)
+     * @param pos
+     * @return 
+     */
+    public boolean caseLibre(Case pos){
+        boolean a;
+        if (pos.getJoueur()==null){
+            a=true;
+        }
+        else{
+            a=false;
+        }
+        return a;
+    }
+    
+    public boolean caseJouable(Case pos){
+        boolean a;
+        a=(caseLibre(pos)||!(horsPlateau(pos.getX(),pos.getY())));
+        if(nombreLibertésAutourDe(pos)<1){
+            a=false;
+        }
+        return a;
+    }
+
+    private int nombreLibertésAutourDe(Case pos) {
+        int n=0;
+        ArrayList<Case> a = new ArrayList();
+            a=getCasesAutourDe(pos);
+            for(int i=0;i<a.size();i++){
+                if(caseLibre(a.get(i))){
+                    n++;
+                }
+            }
+            
+        return n;
+    }
+    
+    
     
 }
